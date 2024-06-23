767986a5d46936a5d07f2b0426d2d3167b72790c
brokenpike <brokenpike@garmr.org>
2024-06-23 19:21:01 +0200

    Homanager usr changed from master to release-24.05

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-24.05";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
